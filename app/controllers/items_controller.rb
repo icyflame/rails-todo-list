006b2f528234cd2b191334feac10a19bcb39c1d9
icyflame <kannan.siddharth12@gmail.com>
2014-11-03 16:07:05 +0530

    Added the todo item adding function.
    Add actions new and create in the items controller.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,27 @@
 class ItemsController < ApplicationController
-  def index
-  end
+	def index
+	end
 
-  def show
-  end
+	def show
+	end
+
+	def new
+		if user_signed_in?
+			@user = current_user
+		else
+			render plain: "Please sign in, before trying to add a Todo Item."
+		end
+	end
+
+	def create
+		@user = User.find(params[:user_id])
+		@item = @user.items.create(item_params)
+		render plain: "Created the todo item."
+		# redirect_to user_path(@user)
+	end
+
+	private
+	def item_params
+		params.require(:item).permit(:title, :content)
+	end
 end
